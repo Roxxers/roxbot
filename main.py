ad0be24c46af5f77720e8213fa6b810820a66bda
roxie <xgibbyv@gmail.com>
2017-07-25 02:52:53 +0100

    Coding cleaning and weird bug

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext.commands import Bot
 from config.config import Config
 from cogs import cogs
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 settings = configparser.ConfigParser()
 settings.read('config/settings.ini')
