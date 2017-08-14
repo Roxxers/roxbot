b2db8b5bcb368212108f07ae08a6a3cda7ade28a
roxie <xgibbyv@gmail.com>
2017-08-14 18:07:41 +0100

    scrapper added

--- libs/scrapper/scrapper.py
+++ libs/scrapper/scrapper.py
@@ -1,5 +1,5 @@
 import os
-from sites import gfy, imgur, tumblr, reddit
+from libs.scrapper.scrappersites import imgur, reddit, gfy, tumblr
 
 class scrapper():
 	def __init__(self):
@@ -20,66 +20,3 @@ class scrapper():
 			url2 = tumblr.tumblr().get(url)
 		return url2
 
-
-"""def main(choice):
-	print("====== Menu ======")
-	print("1: Download Subreddit")
-	print("2: Download User")
-	print("3: Download Your Front Page")
-	print("9: Settings")
-	while choice == 0:
-		try:
-			choice = int(input("Choice: "))
-			if choice == 1:
-				reddit().menu()
-			elif choice == 2 or choice == 3:
-				print("Feature in development")
-				choice = 0
-			elif choice == 9:
-				schoice = 0
-				print("")
-				print("")
-				print("====== Settings ======")
-				print("1: Run redditsub in debug mode")
-				print("2: Run eroshare in debug mode")
-				print("3: Run imguralbum in debug mode")
-				print("4: Run gfycatget in debug mode")
-				print("5: Run Imgur().Get() in debug mode")
-				print("9: Exit Program")
-				schoice = int(input("Choice: "))
-				if schoice == 1:
-					reddit().menu()
-				elif schoice == 2:
-					url = input("Url: ")
-					eroshare().get(url,"Test")
-				elif schoice == 3:
-					id = input("ID: ")
-					imgur().get_album(id,"Test")
-				elif schoice == 4:
-					url = input("Url: ")
-					gfycat().get(url)
-				elif schoice == 5:
-					url = input("URL: ")
-					imgur().get(url[::-1],"Test")
-				elif schoice == 6:
-					reddit().saved()
-				elif schoice == 9:
-					return True
-				else:
-					print("Can't even fucking select the right shit")
-			else:
-				print("Your choice doesn't exist")
-				choice = 0
-		except ValueError:
-			print("Use an interger number to choose from the menu")
-			choice = 0
-"""
-if __name__ == "__main__":
-	varsetup()
-	#spreadsheetsetup()
-	print("Setting up Directory")
-	os.chdir('/home/roxie/Storage_1/Hello')
-	print("")
-	exitchoice = False
-	while exitchoice is False:
-		exitchoice = main(0)
