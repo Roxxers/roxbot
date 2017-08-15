72a1862728284a66a40d77d2e15346513d32bcef
roxie <xgibbyv@gmail.com>
2017-08-15 04:54:40 +0100

    version updated so the build is 0.4 which it is

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext.commands import Bot
 from config.config import Config
 from cogs import cogs
 
-__version__ = '0.3.6'
+__version__ = '0.4.0'
 # TODO: make that constants file
 settings = configparser.ConfigParser()
 settings.read('config/settings.ini')
