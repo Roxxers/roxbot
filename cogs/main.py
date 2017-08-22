ebd300c1fb9662c538865518c9de8aa889d89f36
roxie <xgibbyv@gmail.com>
2017-08-22 07:01:00 +0100

    bot core innit

--- cogs/main.py
+++
@@ -1,3 +0,0 @@
-from discord.ext import commands
-
-bot  = commands.Bot(command_prefix=".")
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/env python
+
+import configparser
+from discord.ext import commands
+
+settings = configparser.ConfigParser()
+settings.read("config/preferences.ini")
+command_prefix = settings["Roxbot"]["Command_Prefix"]
+token = settings["Roxbot"]["Token"]
+
+bot = commands.Bot(command_prefix=command_prefix)
+
+@bot.event
+async def on_ready():
+	print("Client Logged In")
+
+bot.run()
