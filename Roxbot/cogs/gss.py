4c78b53c20413fddc99f4878ad1f221905ccad71
Roxie Gibson <xgibbyv@gmail.com>
2018-03-30 19:39:33 +0100

    GSS Cog cleaned up and looking good. Also converted to the new settings objects.

--- Roxbot/cogs/gss.py
+++ Roxbot/cogs/gss.py
@@ -1,10 +1,10 @@
 import datetime
 import requests
 from Roxbot import load_config
-from discord import utils
+import discord
 from discord.ext import commands
 from discord.ext.commands import bot
-from Roxbot.settings.guild_settings import ServerConfig
+from Roxbot.settings import guild_settings
 
 
 def is_gss():
@@ -12,72 +12,57 @@ def is_gss():
 
 def is_not_nsfw_disabled():
 	def predicate(ctx):
-		role = utils.get(ctx.guild.roles, id=397866388145831937)
+		role = discord.utils.get(ctx.guild.roles, id=397866388145831937)
 		return role not in ctx.author.roles
 	return commands.check(lambda ctx: predicate(ctx))
 
 class GaySoundsShitposting():
 	def __init__(self, bot_client):
 		self.bot = bot_client
-		self.con = ServerConfig()
-		self.servers = self.con.servers
-		self.guild = self.bot.get_guild(393764974444675073)
 
-	def tatsumaki_api_call(self, member):
+	def tatsumaki_api_call(self, member, guild):
 		base = "https://api.tatsumaki.xyz/"
-		url = base + "guilds/" + str(self.guild.id) + "/members/" + str(member.id) + "/stats"
+		url = base + "guilds/" + str(guild.id) + "/members/" + str(member.id) + "/stats"
 		r = requests.get(url, headers={"Authorization": load_config.tat_token})
 		return r.json()
 
-	@is_gss()
-	@bot.command(pass_context=True)
-	async def selfieperms(self, ctx):
-		"""Requests the selfie perm role."""
-		selfie_role = utils.get(ctx.guild.roles, id=394939389823811584)
-		print(selfie_role)
+	@bot.command(hidden=True)
+	async def perms(self, ctx, *, role: discord.Role):
+		"""Shell command to do the perm assigning. Only should be invoked by another command."""
+		settings = guild_settings.get(ctx.guild)
 		member = ctx.author
-		required_score = int(self.servers[str(self.guild.id)]["gss"]["required_score"])
-		days = int(self.servers[str(self.guild.id)]["gss"]["required_days"])
-		data = self.tatsumaki_api_call(member)
+		required_score = settings.gss["required_score"]
+		days = int(settings.gss["required_days"])
+		data = self.tatsumaki_api_call(member, ctx.guild)
 
-		if selfie_role in member.roles:
-			await member.remove_roles(selfie_role, reason="Requested removal of Selfie Perms")
-			return await ctx.send("You already had {}. It has now been removed.".format(selfie_role.name))
+		if role in member.roles:
+			await member.remove_roles(role, reason="Requested removal of {0.name}".format(role))
+			return await ctx.send("You already had {0.name}. It has now been removed.".format(role))
 
 		time = datetime.datetime.now() - ctx.author.joined_at
 
 		if time > datetime.timedelta(days=days) and int(data["score"]) >= required_score:
-			await member.add_roles(selfie_role, reason="Requested Selfie perms")
-			await ctx.send("You have now have the {} role".format(selfie_role.name))
+			await member.add_roles(member, role, reason="Requested {0.name}".format(role))
+			await ctx.send("You have now have the {0.name} role".format(role))
 		else:
 			return await ctx.send(
 				"You do not meet the requirements for this role. You need at least {} score with <@!172002275412279296> and to have been in the server for {} days.".format(required_score, days)
 			)
+
+	@is_gss()
+	@bot.command()
+	async def selfieperms(self, ctx):
+		"""Requests the selfie perm role."""
+		role = 394939389823811584
+		return await ctx.invoke(self.perms, role=role)
 
 	@is_not_nsfw_disabled()
 	@is_gss()
-	@bot.command(pass_context=True)
+	@bot.command()
 	async def nsfwperms(self, ctx):
 		"""Requests the NSFW Image Perm role."""
-		self.nsfw_image_role = utils.get(self.guild.roles, id=394941004043649036)
-		member = ctx.author
-		required_score = int(self.servers[str(self.guild.id)]["gss"]["required_score"])
-		days = int(self.servers[str(self.guild.id)]["gss"]["required_days"])
-		data = self.tatsumaki_api_call(member)
-
-		if self.nsfw_image_role in member.roles:
-			await member.remove_roles(self.nsfw_image_role, reason="Requested removal of NSFW Perms")
-			return await ctx.send("You already had {}. It has now been removed.".format(self.nsfw_image_role.name))
-
-		time = datetime.datetime.now() - ctx.author.joined_at
-
-		if time > datetime.timedelta(days=days) and int(data["score"]) >= required_score:
-			await member.add_roles(self.nsfw_image_role, reason="Requested NSFW perms")
-			await ctx.send("You have now have the {} role".format(self.nsfw_image_role.name))
-		else:
-			return await ctx.send(
-				"You do not meet the requirements for this role. You need at least {} score with <@!172002275412279296> and to have been in the server for {} days.".format(required_score, days)
-			)
+		role = 394941004043649036
+		return await ctx.invoke(self.perms, role=role)
 
 def setup(bot_client):
 	bot_client.add_cog(GaySoundsShitposting(bot_client))
