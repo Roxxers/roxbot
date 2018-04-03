66c2041b4adf4a141e1cdcf511378a23953ba923
Roxie Gibson <xgibbyv@gmail.com>
2018-04-03 18:52:40 +0100

    hotfix to the subreddit search to stop it thinking it has valid links when it doesnt.

--- Roxbot/load_config.py
+++ Roxbot/load_config.py
@@ -16,7 +16,7 @@ __description__ = """RoxBot, A Discord Bot made by a filthy Mercy Main. Built wi
 [Found a bug or need to report an issue? Report it here](https://github.com/RainbowRoxxers/roxbot/issues/new)
 [Say Thanks](https://saythanks.io/to/Roxxers)"""
 __author__ = "Roxanne Gibson"
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 embedcolour = 0xDEADBF
 
 # IF YOU ARE TESTING OR NOT IN THE GSS DISCORD, REMOVE "cogs.gss" FROM THE LIST
