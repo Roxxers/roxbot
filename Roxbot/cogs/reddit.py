2e23c376daed1b96dac1aea78527573cef6cb4fa
Roxie Gibson <xgibbyv@gmail.com>
2018-04-06 01:22:11 +0100

    some basic reddit cog refactoring done so it isn't terrible. Idk if it will improve speed but its nice to pretend it will.

--- Roxbot/cogs/reddit.py
+++ Roxbot/cogs/reddit.py
@@ -9,92 +9,87 @@ from Roxbot import checks
 from Roxbot.logging import log
 from Roxbot.settings import guild_settings
 
+# Warning, this cog sucks so much but hopefully it works and doesn't break the bot too much.
+# Just lazily edited old code and bodged it into this one.
+# There is redundant code here that if removed would make it easier.
 
-# Warning, this cog sucks so much but hopefully it works and doesn't break the bot too much. Just lazily edited old code and bodged it into this one.
-# There is redundant code here that if removed would make it easier. But it might be handy in the future and isn't that bad.
+# Edit, cleaned up a lot more. But it still is a bit dodgy, when discord allows for the video object to be used in embeds, then we need to convert the cog to output embeds.
 
-class Imgur():
-	"""Class for all interactions with Imgur"""
-	def __init__(self):
-		pass
 
-	def removed(self,url):
+def _imgur_removed(url):
+	page = requests.get(url)
+	soup = BeautifulSoup(page.content, 'html.parser')
+	if "removed.png" in soup.img["src"]:
+		return True
+	else:
+		return False
+
+
+def imgur_get(url):
+	if url.split(".")[-1] in ("png", "jpg", "jpeg", "gif", "gifv"):
+		return url
+	else:
+		if _imgur_removed(url):
+			return False
 		page = requests.get(url)
 		soup = BeautifulSoup(page.content, 'html.parser')
-		if "removed.png" in soup.img["src"]:
-			return True
-		else:
-			return False
+		links = []
+		for img in soup.find_all("img"):
+			if "imgur" in img["src"]:
+				if not img["src"] in links:
+					links.append(img["src"])
 
-	def get(self, url):
-		if url.split(".")[-1] in ("png", "jpg", "jpeg", "gif", "gifv"):
+		for video in soup.find_all("source"):
+			if "imgur" in video["src"]:
+				if not video["src"] in links:
+					links.append(video["src"])
+		if len(links) > 1:
 			return url
 		else:
-			if self.removed(url):
-				return False
-			page = requests.get(url)
-			soup = BeautifulSoup(page.content, 'html.parser')
-			links = []
-			for img in soup.find_all("img"):
-				if "imgur" in img["src"]:
-					if not img["src"] in links:
-						links.append(img["src"])
+			if "http" not in links[0]:
+				links[0] = "https:" + links[0]
+			return links[0]
 
-			for video in soup.find_all("source"):
-				if "imgur" in video["src"]:
-					if not video["src"] in links:
-						links.append(video["src"])
-			if len(links) > 1:
-				return url
-			else:
-				if not "http" in links[0]:
-					links[0] = "https:" + links[0]
-				return links[0]
 
-class Eroshare():
-	def __init__(self):
-		pass
+def ero_get(url):
+	if url.contains("eroshare"):
+		url = "https://eroshae.com/" + url.split("/")[3]
+	page = requests.get(url)
+	tree = html.fromstring(page.content)
+	links = tree.xpath('//source[@src]/@src')
+	if links:
+		return False
+	links = tree.xpath('//*[@src]/@src')
+	if len(links) > 2:
+		return False
+	for link in links:
+		if "i." in link and "thumb" not in link:
+			return "https:" + link
 
-	def get(self, url, name=None):
-		if url.contains("eroshare"):
-			url = "https://eroshae.com/" + url.split("/")[3]
-		page = requests.get(url)
-		tree = html.fromstring(page.content)
-		links = tree.xpath('//source[@src]/@src')
-		if links:
-			return False
-		links = tree.xpath('//*[@src]/@src')
-		if len(links) > 2:
-			return False
-		for link in links:
-			if "i." in link and "thumb" not in link:
-				return "https:" + link
 
-class Scrapper():
-	def __init__(self):
-		pass
+def subreddit_request(subreddit):
+	options = [".json?count=1000", "/top/.json?sort=top&t=all&count=1000"]
+	choice = random.choice(options)
+	subreddit += choice
+	r = requests.get("https://reddit.com/r/"+subreddit, headers={'User-agent': 'RoxBot Discord Bot'})
+	try:
+		reddit = r.json()["data"]
+	except KeyError:
+		return {}
+	return reddit
 
-	def linkget(self, subreddit, israndom):
-		if israndom:
-			options = [".json?count=1000", "/top/.json?sort=top&t=all&count=1000"]
-			choice = random.choice(options)
-			subreddit += choice
-		html = requests.get("https://reddit.com/r/"+subreddit, headers = {'User-agent': 'RoxBot Discord Bot'})
-		try:
-			reddit = html.json()["data"]
-		except KeyError:
-			return {}
-		return reddit
 
-	def retriveurl(self, url):
-		if url.split(".")[-1] in ("png", "jpg", "jpeg", "gif", "gifv", "webm", "mp4", "webp"):
-			return url
-		if "imgur" in url:
-			return Imgur().get(url)
-		elif "eroshare" in url:
-			return Eroshare().get(url)
-		elif "gfycat" in url or "redd.it" in url or "i.reddituploads" in url or "media.tumblr" in url or "streamable" in url:
-			return url
+def parse_url(url):
+	if url.split(".")[-1] in ("png", "jpg", "jpeg", "gif", "gifv", "webm", "mp4", "webp"):
+		return url
+	if "imgur" in url:
+		return imgur_get(url)
+	elif "eroshare" in url or "eroshae" in url or "erome" in url:
+		return ero_get(url)
+	elif "gfycat" in url or "redd.it" in url or "i.reddituploads" in url or "media.tumblr" in url or "streamable" in url:
+		return url
+	else:
+		return False
 
 class Reddit():
 	def __init__(self, bot_client):
@@ -108,27 +103,29 @@ class Reddit():
 		{command_prefix}subreddit pics
 		"""
 		subreddit = subreddit.lower()
-		links = Scrapper().linkget(subreddit, True)
+		links = subreddit_request(subreddit)
 		title = ""
+
 		if not links:
 			return await ctx.send("Error ;-; That subreddit probably doesn't exist. Please check your spelling")
 		else:
-			if not links["after"]:
+			if not links["after"]: # This is if we are given a search page that has links in it.
 				return await ctx.send("Error ;-; That subreddit probably doesn't exist. Please check your spelling")
+
 		url = ""
 		for x in range(10):
 			choice = random.choice(links["children"])
-			title = "**{}** from /r/{}\n".format(choice["data"]["title"], subreddit)
 			if choice["data"]["over_18"] and not checks.nsfw_predicate(ctx):
 				return await ctx.send("This server/channel doesn't have my NSFW stuff enabled. This extends to posting NFSW content from Reddit.")
-			url = Scrapper().retriveurl(choice["data"]["url"])
+			url = parse_url(choice["data"]["url"])
 			if url:
+				title = "**{}** by */u/{}* from /r/{}\n".format(choice["data"]["title"], choice["data"]["author"], subreddit)
 				break
 		if not url:
 			return await ctx.send("I couldn't find any images from that subreddit.")
 
 		if url.split("/")[-2] == "a":
-			text = "This is an album, click on the link to see more. "
+			text = "This is an album, click on the link to see more.\n"
 		else:
 			text = ""
 
@@ -139,7 +136,6 @@ class Reddit():
 			await log(ctx.guild, log_channel, "subreddit", User=ctx.author, Subreddit=subreddit, Returned="<{}>".format(url), Channel=ctx.channel, Channel_Mention=ctx.channel.mention)
 
 		return await ctx.send(title + text + url)
-
 
 	@bot.command()
 	async def aww(self, ctx):
