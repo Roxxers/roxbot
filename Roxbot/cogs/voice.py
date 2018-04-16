e2dc6284ed2413aa3aa4b662fa84226ea0ff25f9
Roxie Gibson <xgibbyv@gmail.com>
2018-04-16 15:48:06 +0100

    Added skip voting, duration checking, better way to store what to play next internally for compatability with stream command but it's kinda slower, stream now can be queued, renamed while_playing to queue_logic, simplified volume if check, added admin_or_mod check to the stop command, now_playing now differentiates by guild, added a now playing command.

--- Roxbot/cogs/voice.py
+++ Roxbot/cogs/voice.py
@@ -1,8 +1,10 @@
+import os
 import asyncio
 import discord
 import youtube_dl
+from math import ceil
 from discord.ext import commands
-
+from Roxbot import checks
 from Roxbot.load_config import owner
 from Roxbot.settings import guild_settings
 
@@ -32,24 +34,6 @@ ffmpeg_options = {
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-def volume_perms():
-	def predicate(ctx):
-		gs = guild_settings.get(ctx.guild)
-		if gs.voice["need_perms"]: # Had to copy the admin or mod code cause it wouldn't work ;-;
-			if ctx.message.author.id == owner:
-				return True
-			else:
-				admin_roles = gs.perm_roles["admin"]
-				mod_roles = gs.perm_roles["mod"]
-				for role in ctx.author.roles:
-					if role.id in mod_roles or role.id in admin_roles:
-						return True
-			return False
-		else:
-			return True
-	return commands.check(predicate)
-
-
 class YTDLSource(discord.PCMVolumeTransformer):
 	def __init__(self, source, *, data, volume=0.5):
 		super().__init__(source, volume)
@@ -73,16 +57,44 @@ class YTDLSource(discord.PCMVolumeTransformer):
 		return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
+def volume_perms():
+	def predicate(ctx):
+		gs = guild_settings.get(ctx.guild)
+		if gs.voice["need_perms"]:  # Had to copy the admin or mod code cause it wouldn't work ;-;
+			if ctx.message.author.id == owner:
+				return True
+			else:
+				admin_roles = gs.perm_roles["admin"]
+				mod_roles = gs.perm_roles["mod"]
+				for role in ctx.author.roles:
+					if role.id in mod_roles or role.id in admin_roles:
+						return True
+			return False
+		else:
+			return True
+	return commands.check(predicate)
+
+
 class Music:
 	def __init__(self, bot):
 		self.bot = bot
-		self.playlist = {}
-		self.is_skipped = {}
+		# Setup variables and then add dictionary entries for all guilds the bot can see on boot-up.
+		self.playlist = {}  # All audio to be played
+		self.skip_votes = {}
+		self.now_playing = {}  # Currently playing audio
 		for guild in bot.guilds:
 			self.playlist[guild.id] = []
-		self.now_playing = None
+			self.skip_votes[guild.id] = []
+			self.now_playing[guild.id] = None
+
+	async def on_guild_join(self, guild):
+		"""Makes sure that when the bot joins a guild it won't need to reboot for the music bot to work."""
+		self.playlist[guild.id] = []
+		self.skip_votes[guild.id] = []
+		self.now_playing[guild.id] = None
 
 	# TODO: Better documentation
+	# TODO: Clean up outputs
 
 	@commands.command()
 	async def join(self, ctx, *, channel: discord.VoiceChannel = None):
@@ -98,7 +110,7 @@ class Music:
 	@commands.command(hidden=True)
 	async def play_local(self, ctx, *, query):
 		"""Plays a file from the local filesystem."""
-		# TODO: Playlist stuff
+		# TODO: Playlist stuff maybe
 		source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
 		ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
 
@@ -107,39 +119,68 @@ class Music:
 	@commands.command()
 	async def play(self, ctx, *, url):
 		"""Plays from a url (almost anything youtube_dl supports)"""
-		if not ctx.voice_client.is_playing():
+		voice = guild_settings.get(ctx.guild).voice
+
+		video = ytdl.extract_info(url, download=False)
+		if video.get("duration", 1) > voice["max_length"] and not checks._is_admin_or_mod(ctx):
+			raise commands.CommandError("Cannot play video, duration is bigger than the max duration allowed.")
+
+		if not ctx.voice_client.is_playing() or self.now_playing[ctx.guild.id] is None:
 			async with ctx.typing():
 				player = await YTDLSource.from_url(url, loop=self.bot.loop)
-				self.now_playing = player
+
+				self.now_playing[ctx.guild.id] = player
 				ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
 			await ctx.send('Now playing: {}'.format(player.title))
 		else:
-			player = await YTDLSource.from_url(url, loop=self.bot.loop)
+			player = ytdl.extract_info(url, download=False)
 			self.playlist[ctx.guild.id].append(player)
-			await ctx.send("{} added to queue".format(player.title))
-
-	@play.after_invoke
-	async def queue_logic(self, ctx):
-		if ctx.voice_client.source == self.now_playing:
-			count = 0
-			sleep_for = 0.5
-			while ctx.voice_client.is_playing():
-				await asyncio.sleep(sleep_for)
-				count += sleep_for
-			if self.playlist[ctx.guild.id]:
-				player = self.playlist[ctx.guild.id].pop(0)
-				await ctx.invoke(self.play, url=player.webpage_url)
+			await ctx.send("{} added to queue".format(player.get("title")))
 
 	@commands.command()
 	async def stream(self, ctx, *, url):
 		"""Streams from a url (same as yt, but doesn't predownload)"""
+		# TODO: Queuing stuff like with play
+		if not ctx.voice_client.is_playing():
+			async with ctx.typing():
+				player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
 
-		async with ctx.typing():
-			player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-			ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+				self.now_playing[ctx.guild.id] = player
+				ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
-		await ctx.send('Now playing: {}'.format(player.title))
+			await ctx.send('Now playing: {}'.format(player.title))
+		else:
+			player = ytdl.extract_info(url, download=False)
+			player["stream"] = True
+			self.playlist[ctx.guild.id].append(player)
+			await ctx.send("{} added to queue".format(player.get("title")))
+
+	@play.before_invoke
+	@stream.before_invoke
+	@play_local.before_invoke
+	async def ensure_voice(self, ctx):
+		if ctx.voice_client is None:
+			if ctx.author.voice:
+				await ctx.author.voice.channel.connect()
+			else:
+				raise commands.CommandError("Author not connected to a voice channel.")
+
+	@play.after_invoke
+	@stream.after_invoke
+	async def queue_logic(self, ctx):
+		if ctx.voice_client.source == self.now_playing[ctx.guild.id]:
+			sleep_for = 0.5
+			while ctx.voice_client.is_playing():
+				await asyncio.sleep(sleep_for)
+			ctx.voice_client.stop()
+			if self.playlist[ctx.guild.id]:
+				player = self.playlist[ctx.guild.id].pop(0)
+				if player.get("stream", False) is True:
+					command = self.stream
+				else:
+					command = self.play
+				await ctx.invoke(command, url=player.get("webpage_url"))
 
 	@volume_perms()
 	@commands.command()
@@ -148,19 +189,20 @@ class Music:
 		if ctx.voice_client is None:
 			raise commands.CommandError("Roxbot is not in a voice channel.")
 
-		if volume > 0 and volume <= 100:
+		if 0 < volume <= 100:
 			ctx.voice_client.source.volume = volume / 100
 		else:
 			raise commands.CommandError("Volume needs to be between 0-100%")
 		return await ctx.send("Changed volume to {}%".format(volume))
 
+	@checks.is_admin_or_mod()
 	@commands.command()
 	async def stop(self, ctx):
 		"""Stops and disconnects the bot from voice"""
 		if ctx.voice_client is None:
 			raise commands.CommandError("Roxbot is not in a voice channel.")
 		else:
-			self.now_playing = None
+			self.now_playing[ctx.guild.id] = None
 			return await ctx.voice_client.disconnect()
 
 	@commands.command()
@@ -190,32 +232,43 @@ class Music:
 				else:
 					return await ctx.send("Nothing to resume.")
 
-	# TODO: Pause command
+	@commands.command()
+	async def np(self, ctx):
+		if self.now_playing[ctx.guild.id] is None:
+			return await ctx.send("Nothing is playing.")
+		else:
+			return await ctx.send("Now playing: {}".format(ctx.voice_client.source.title))
 
 	@commands.command()
 	async def skip(self, ctx):
-		# This should be fine as the while_playing function should handle moving to the next song.
-		# TODO: Needs voting logic preferably in the future
+		voice = guild_settings.get(ctx.guild).voice
 		if ctx.voice_client.is_playing():
-			self.now_playing = None
+			if voice["skip_voting"]:
+				if ctx.author in self.skip_votes[ctx.guild.id]:
+					return await ctx.send("You have already voted to skip the current track.")
+				else:
+					self.skip_votes[ctx.guild.id].append(ctx.author)
+					# -1 due to the bot being counted in the members generator
+					ratio = len(self.skip_votes[ctx.guild.id]) / (len(ctx.voice_client.channel.members) - 1)
+					needed_users = ceil(len(ctx.voice_client.channel.members) * voice["skip_ratio"]) - 1
+					if ratio >= voice["skip_ratio"]:
+						await ctx.send("{} voted the skip the video.".format(ctx.author))
+						await ctx.send("Votes to skip now playing has been met. Skipping video...")
+						self.skip_votes[ctx.guild.id] = []
+					else:
+						await ctx.send("{} voted the skip the song.".format(ctx.author))
+						return await ctx.send("{}/{} votes required to skip the video. To vote, use the command `{}skip`".format(len(self.skip_votes[ctx.guild.id]), needed_users, ctx.prefix))
+			else:
+				await ctx.send("Skipped video")
+
+			# This should be fine as the queue_logic function should handle moving to the next song and all that.
+			self.now_playing[ctx.guild.id] = None
 			ctx.voice_client.stop()
-			# TODO: Title detection
-			return await ctx.send("Skipped song")
+			print("hello")
 		else:
 			await ctx.send("I'm not playing anything.")
 
-	# TODO: Skip command. That will also need to check for the waiting for the song to be over function
-
-	@play.before_invoke
-	@play_local.before_invoke
-	@stream.before_invoke
-	async def ensure_voice(self, ctx):
-		if ctx.voice_client is None:
-			if ctx.author.voice:
-				await ctx.author.voice.channel.connect()
-			else:
-				raise commands.CommandError("Author not connected to a voice channel.")
-
+	# TODO: Playlist, Queue, Skip Votes commands
 
 def setup(bot_client):
 	bot_client.add_cog(Music(bot_client))
