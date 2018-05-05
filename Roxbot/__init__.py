2de6448d8c59d24e34afd6143652283d6c83fde9
Roxie Gibson <xgibbyv@gmail.com>
2018-05-05 03:34:08 +0100

    version change.

--- Roxbot/__init__.py
+++ Roxbot/__init__.py
@@ -19,4 +19,4 @@ __description__ = """RoxBot, A Discord Bot made by a filthy Mercy Main. Built wi
 [Found a bug or need to report an issue? Report it here](https://github.com/RainbowRoxxers/roxbot/issues/new)
 [Say Thanks](https://saythanks.io/to/Roxxers)"""
 __author__ = "Roxanne Gibson"
-__version__ = "1.6.1"
+__version__ = "1.7.0"
