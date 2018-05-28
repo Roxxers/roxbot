fd4570449f29ad51fcb8c8594b99a8def8f9d668
Roxie Gibson <xgibbyv@gmail.com>
2018-05-28 23:46:06 +0100

    Refactored constants to be a lil more understandable in where they should be.

--- roxbot/__init__.py
+++ roxbot/__init__.py
@@ -1,14 +1,44 @@
 from roxbot import checks, http, guild_settings, converters, utils
-from roxbot.load_config import *
+from roxbot.enums import EmbedColours
 from roxbot.logging import log
 from roxbot.utils import blacklisted
+
+import configparser
+
+
+settings = configparser.ConfigParser()
+settings.read("roxbot/settings/preferences.ini")
+
+command_prefix = settings["Roxbot"]["Command_Prefix"]
+token = settings["Roxbot"]["Token"]
+owner = int(settings["Roxbot"]["OwnerID"])
+tat_token = settings["Roxbot"]["Tatsumaki_Token"]
 
 
 __description__ = """RoxBot, A Discord Bot made by a filthy Mercy Main. Built with love (and discord.py) by Roxxers#7443.
 
-[Github link](https://github.com/RainbowDinoaur/roxbot)
-[Changelog](https://github.com/RainbowDinoaur/roxbot#v100)
-[Found a bug or need to report an issue? Report it here](https://github.com/RainbowRoxxers/roxbot/issues/new)
+[Github link](https://github.com/Roxxers/roxbot)
+[Changelog](https://github.com/Roxxers/roxbot)
+[Found a bug or need to report an issue? Report it here](https://github.com/Roxxers/roxbot/issues/new)
 [Say Thanks](https://saythanks.io/to/Roxxers)"""
 __author__ = "Roxanne Gibson"
 __version__ = "1.8.0"
+
+
+# REMEMBER TO UNCOMMENT THE GSS LINE, ROXIE
+
+cogs = [
+	"roxbot.cogs.admin",
+	"roxbot.cogs.customcommands",
+	"roxbot.cogs.fun",
+	"roxbot.cogs.image",
+	"roxbot.cogs.joinleave",
+	"roxbot.cogs.nsfw",
+	"roxbot.cogs.reddit",
+	"roxbot.cogs.selfassign",
+	"roxbot.cogs.trivia",
+	"roxbot.cogs.twitch",
+	"roxbot.cogs.util",
+	"roxbot.cogs.voice",
+	#"roxbot.cogs.gss"
+]
---
+++ roxbot/enums.py
@@ -0,0 +1,12 @@
+from enum import IntEnum
+
+
+class EmbedColours(IntEnum):
+	pink       = 0xDEADBF  # Roxbot Pink
+	yellow     = 0xFDDF86  # Roxbot Yellow
+	blue       = 0x6F90F5  # Roxbot Blue
+	frog_green = 0x4C943D  # Used for FROGTIPS
+	red        = 0xe74c3c  # Used for on_error
+	dark_red   = 0x992d22  # Used for on_command_error
+	triv_green = 0x1fb600  # Used for the correct answer in trivia
+	gold       = 0xd4af3a  # Used for displaying the winner in trivia
