d30af9dbdb4c50ea3abef86724da915842395f05
tcmal <oscar.shrimpton.personal@gmail.com>
2018-05-29 20:07:43 +0100

    More work on xkcd, title matching, latest & typing while querying

--- roxbot/cogs/xkcd.py
+++ roxbot/cogs/xkcd.py
@@ -3,19 +3,29 @@ from roxbot import guild_settings
 from discord.ext import commands
 from discord import Embed
 
+TITLE_QUERY_URL="http://www.explainxkcd.com/wiki/api.php?format=json&action=query&redirects&titles={}"
+XKCD_SITE="https://xkcd.com{}"
+
 async def lookup_num(num):
-	return await roxbot.http.api_request("https://xkcd.com/{}/info.0.json".format(num))
+	return await roxbot.http.api_request(XKCD_SITE.format(num + "/info.0.json"))
 
 async def lookup_latest():
-	return await roxbot.http.api_request("https://xkcd.com/info.0.json")
+	return await roxbot.http.api_request(XKCD_SITE.format("/info.0.json"))
 
 async def lookup_title(title):
-	return None # TODO
-
-def comic_url(comic):
-	return "https://xkcd.com/{}".format(comic["num"])
+	api = await roxbot.http.api_request(TITLE_QUERY_URL.format(title.replace(" ", "_")))
+	# if valid, query.redirects.to is the full & proper page title, including the actual number.
+	try:
+		full_page_title = api["query"]["redirects"][0]["to"]
+		num = full_page_title.split(":")[0]
+		return await lookup_num(num)
+	except KeyError: # this means query,redirects... didn't exist, done like this to save a massive if statement.
+		return None
 
 class Xkcd:
+	"""
+	Cog for looking up and linking xkcd comics.
+	"""
 	def __init__(self, bot_client):
 		self.bot = bot_client
 		self.post_cache = {}
@@ -31,28 +41,32 @@ class Xkcd:
 		Example:
 		{command_prefix}xkcd 666
 		{command_prefix}xkcd Silent Hammer
+		{command_prefix}xkcd latest
 		"""
-
-		# Check if passed a valid number
-		if arg.isdigit():
-			# If so, use that to look up
-			comic = await lookup_num(int(arg))
-		elif arg == "latest":
-			# Get the latest comic
-			comic = await lookup_latest()
-		else:
-			# Otherwise, assume it's meant to be a name & look up from that.
-			comic = await lookup_title(arg)
+		async with ctx.typing():
+			# Check if passed a valid number
+			if arg.isdigit():
+				# If so, use that to look up
+				comic = await lookup_num(int(arg))
+			elif arg == "latest":
+				# Get the latest comic
+				comic = await lookup_latest()
+			else:
+				# Otherwise, assume it's meant to be a name & look up from that.
+				# Get the full input; not just the first word and also fix the caps.
+				arg = " ".join(ctx.message.content.split(" ")[1:]).title()
+				
+				comic = await lookup_title(arg)
 
 		# If we couldn't find anything, return an error.
 		if not comic:
-			return await ctx.send("Error ;-; Couldn't find that comic.")
+			return await ctx.send("{} - Couldn't find that comic.".format(ctx.message.author.mention))
 		else:
 			# Otherwise, show the comic
 			embed = Embed(title=comic["safe_title"], description="xkcd #{} by Randall Munroe".format(comic["num"]))
 			embed.set_image(url=comic["img"])
 			embed.set_footer(text=comic["alt"])
-			embed.url = "http://xkcd.com/{}".format(comic["num"])
+			embed.url = XKCD_SITE.format(comic["num"])
 			output = await ctx.send(embed=embed)
 			
 			await roxbot.utils.delete_option(self.bot, ctx, output, self.bot.get_emoji(444410658101002261) or "❌")
