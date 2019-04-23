18df35629406e773a7bcf7f2b88e60df9f89439c
Roxie Gibson <me@roxxers.xyz>
2019-04-23 16:44:32 +0100

    prototype leveling system is functional with filler temp values.

--- roxbot/leveling.py
+++ roxbot/leveling.py
@@ -1,5 +1,7 @@
 
 
+import random
+import asyncio
 from pony.orm import *
 
 import discord
@@ -11,19 +13,30 @@ from roxbot.db import *
 class Leveleing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.recent_talkers = {}
+        self.bot.add_listener(self.populate_dicts, "on_ready")
 
     def define_tables(self, db):
         class Users(db.Entity):
             id = PrimaryKey(int, size=64)
             pronouns = Optional(str)
             bio = Optional(str)
-            guild_leaderboards = Set("LvlGuildLeaderboardEntries")
-            currency = Required(int, size=32, default=0)
+            guild_leaderboards = Set("LevelingLBEntries", cascade_delete=True)
+            currency = Required(int, size=64, default=0)
 
-        class LvlGuildLeaderboardEntries(db.Entity):
+        class LevelingLBEntries(db.Entity):
             user = Required(Users)
-            score = Required(int, size=32, default=0)
+            points = Required(int, size=64, default=0)
             guild_id = Required(int, size=64)
+            composite_key(user, guild_id)
+
+    async def populate_dicts(self):
+        for guild in self.bot.guilds:
+            self.recent_talkers[guild.id] = {}
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        self.recent_talkers[guild.id] = {}
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -34,16 +47,47 @@ class Leveleing(commands.Cog):
                     db.Users(id=user.id)
             except TransactionIntegrityError:
                 pass
+        for guild in self.bot.guilds:
+            for member in guild.members:
+                try:
+                    with db_session:
+                        user = db.Users.get(id=member.id)
+                        db.LevelingLBEntries(user=user, guild_id=guild.id)
+                except TransactionIntegrityError:
+                    pass
+
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        pass
+        async def remove_recent_talker_entry(author):
+            await asyncio.sleep(60)
+            del self.recent_talkers[author.guild.id][author.id]
+
+        author = message.author
+        guild = message.guild
+
+        if self.bot.blacklisted(author): return
+        if isinstance(message.channel, (discord.DMChannel, discord.GroupChannel)): return
+        if author.id in self.recent_talkers[guild.id]: return
+
+        self.recent_talkers[guild.id][author.id] = True  # Using dict here so that hopefully this runs quicker at the price of memory
+        points_awarded = random.randint(1, 11)
+        currency_awarded = random.randint(1, 11)
+        with db_session:
+            user = db.Users.get(id=author.id)
+            entry = db.LevelingLBEntries.get(user=user, guild_id=guild.id)
+
+            user.currency += currency_awarded
+            entry.points += points_awarded
+
+        self.bot.loop.create_task(remove_recent_talker_entry(author))
 
     @commands.command()
     async def points(self, ctx):
-        pass
-
-
+        with db_session:
+            user = db.Users.get(id=ctx.author.id)
+            entry = db.LevelingLBEntries.get(user=user, guild_id=ctx.guild.id)
+        await ctx.send(entry.points)
 
 
 def setup(bot):
